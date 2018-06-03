0a6057d291e2f483d7bde776a019b226c66bf1eb
Ciara Mo <cicimotl@gmail.com>
2018-06-03 19:25:00 -0400

    UPDATED 6/3/18

--- shop/settings.py
+++ shop/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
 	'accounts',
 	'users',
 	'crispy_forms',
-	'pages'
 ]
 
 MIDDLEWARE = [
--- shop/urls.py
+++ shop/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 	path('accounts/', include('django.contrib.auth.urls')),	
 	path('accounts/', include('accounts.urls')),
-	path('', include('pages.urls')),
 	path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
 ]
